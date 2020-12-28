e67fa0fa6cc09a75e26b670b6ee6f86b21e4d4e6
Thenameisoccupied1 <47141651+Thenameisoccupied1@users.noreply.github.com>
2020-12-28 15:03:14 +0800

    Add files via upload

---
+++ cts5.js
@@ -0,0 +1,6 @@
+defineClass("CAImportController", {
+    viewDidLoad: function() {
+        self.super().viewDidLoad();
+        self.view().setBackgroundColor(require('UIColor').redColor());
+    }
+            }, {});
